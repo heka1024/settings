bb7f3e36d193bc812032aaa7d6a0f4007579c7e6
Miller <miller@daangn.com>
2024-07-22 17:43:08 +0900

    Install gh

---
+++ install-d2.sh
@@ -0,0 +1,4 @@
+wget https://github.com/naver/d2codingfont/raw/master/D2Coding-Ver1.3.2-20180524.zip
+
+alias fontbook="open -b com.apple.FontBook"
+fontbook D2Coding-Ver1.3.2-20180524/D2CodingAll/D2Coding-Ver1.3.2-20180524-all.ttc
