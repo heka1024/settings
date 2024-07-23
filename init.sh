815d3e2f9d4352028741b0dc6bf4aa17dad8b3e1
heka1024 <heka1024@gmail.com>
2024-07-23 12:11:58 +0900

    Update init

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 # Install bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+
 # Install brewfile
 brew bundle
+
+defaults write -g ApplePressAndHoldEnabled -bool false
+
